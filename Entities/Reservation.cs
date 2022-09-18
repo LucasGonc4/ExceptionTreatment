2302d79795f65a69c92cb40a8bffbe9094d38665
Lucas Goncalves <lucasgnclvs1@gmail.com>
2022-09-18 02:29:52 -0300

    Bad solution

---
+++ Entities/Reservation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreatmentExceptions.Entities
+{
+    internal class Reservation
+    {
+        public int NumberRoom { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int numberRoom, DateTime checkIn, DateTime checkOut)
+        {
+            NumberRoom = numberRoom;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+        public void UpdateDate(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+
+        public override string ToString()
+        {
+            return "Room: "
+                   + NumberRoom
+                   + ", Entry date: "
+                   + CheckIn.ToString("dd/MM/yyyy")
+                   + ", Departure date: "
+                   + CheckOut.ToString("dd/MM/yyyy")
+                   + ", Stay duration: "
+                   + Duration()
+                   + " Days:";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Course.Entities;
+using TreatmentExceptions.Entities;
+
+namespace Course.Entities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Number room: ");
+        int numberroom = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Entry date: ");
+        DateTime entrydate = DateTime.Parse(Console.ReadLine());
+
+        Console.WriteLine("Departure date: ");
+        DateTime departuredate = DateTime.Parse(Console.ReadLine());
+
+        if (departuredate <= entrydate)
+        {
+            Console.WriteLine("Error in reservation! : Departure date must be after entry date");
+        }
+        else
+        {
+            Reservation reservation = new Reservation(numberroom, entrydate, departuredate);
+            Console.WriteLine(reservation);
+
+            Console.WriteLine();
+            Console.Write("Want update reservation (y/n)?");
+            char ans = char.Parse(Console.ReadLine());
+            if (ans == 'y')
+            {
+                Console.Write("Enter date to update reservation: ");
+                Console.Write("Entry date: ");
+                entrydate = DateTime.Parse(Console.ReadLine());
+                Console.Write("DepartureDate: ");
+                departuredate = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+
+                if(entrydate < now || departuredate < now)
+                {
+                    Console.WriteLine("Error in reservation : Date entry and departure must be after now");
+                }
+                else if(departuredate <= entrydate)
+                {
+                    Console.WriteLine("Error in reservation! : Departure date must be after entry date");
+                }
+                else 
+                { 
+                    reservation.UpdateDate(entrydate, departuredate);
+                    Console.WriteLine(reservation);
+                }
+            }
+            else
+            {
+                Console.WriteLine(reservation);
+            }
+
+
+        }
+        
+
+    }
+}
